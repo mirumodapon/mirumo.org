4e55fc3630609f7364e9e54385b71be40c2deece
Mirumo <mysteryperson520@gmail.com>
2024-03-10 15:26:10 +0800

    Feat: add ga and Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from 'react';
 import type { Metadata } from 'next';
+import { GoogleAnalytics } from '@next/third-parties/google'
 import 'tailwindcss/tailwind.css';
 import '@/assets/styles/global.css';
+
 
 interface Props {
   children: Readonly<ReactNode>;
@@ -14,6 +16,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="zh-TW">
+      <head>
+        <GoogleAnalytics gaId="G-V4Z00NVGRT" />
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "ledccdbfas");`
+        }}></script>
+      </head>
       <body>
         <template
           dangerouslySetInnerHTML={{
